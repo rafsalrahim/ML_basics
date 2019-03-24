ec9bdd63c61c19744aa8e12e682de57c0617d270
rafsal <rafsalkrahim@gmail.com>
2019-03-24 16:24:39 +0530

    ignore file added

--- test.py
+++ test.py
@@ -3,3 +3,5 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn import preprocessing
 from sklearn import tree
+dataset_url = 'http://mlr.cs.umass.edu/ml/machine-learning-databases/wine-quality/winequality-red.csv'
+data = pd.read_csv(dataset_url, sep=';')
