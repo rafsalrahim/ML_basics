eeaa515aa3a28c0d33f3269a9bb7279d652cdb3e
rafsal <rafsalkrahim@gmail.com>
2019-03-24 16:35:12 +0530

    added

--- test.py
+++ test.py
@@ -6,3 +6,5 @@ from sklearn import tree
 dataset_url = 'http://mlr.cs.umass.edu/ml/machine-learning-databases/wine-quality/winequality-red.csv'
 data = pd.read_csv(dataset_url, sep=';')
 print (data)
+y = data.quality
+X = data.drop('quality', axis=1)
